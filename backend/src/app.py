4aa674de855e50139b46ac9d6790adf0c94e3619
bautistamarrese-hub <bautista.marrese@philips.edu.ar>
2026-08-28 10:34:51 -0300

    conexion local

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,18 @@
 from .routers import desarollador_router, juego_router, usuario_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.middlewares.error_middleware import ErrorHandlerMiddleware
 from src.utils.errors import AppError
 
 app = FastAPI(title="Initial Structure API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_exception_handler(AppError, ErrorHandlerMiddleware)
 
